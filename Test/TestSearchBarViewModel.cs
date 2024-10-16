1b6edb69221930cb3710748202bff25eade47adf
Ankushd98 <ankdwi29@gmail.com>
2024-10-16 22:07:36 +0530

    Changed publish.yml, and added few unit tests.

---
+++ Test/TestSearchBarViewModel.cs
@@ -0,0 +1,82 @@
+﻿using Moq;
+using NUnit.Framework;
+using Util.Events;
+using VideoExplorer;
+
+namespace Test;
+
+public class TestSearchBarViewModel
+{
+    private SearchBarViewModel _viewModel;
+    private Mock<IRegionManager> _regionManagerMock;
+    private Mock<IEventAggregator> _eventAggregatorMock;
+    private Mock<SearchRequestedEvent> _searchRequestedEventMock;
+    private Mock<RefreshRequestedEvent> _refreshRequestedEventMock;
+    private Mock<ClearSearchBarEvent> _clearSearchBarEventMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _regionManagerMock = new Mock<IRegionManager>();
+        _eventAggregatorMock = new Mock<IEventAggregator>();
+        _searchRequestedEventMock = new Mock<SearchRequestedEvent>();
+        _refreshRequestedEventMock = new Mock<RefreshRequestedEvent>();
+        _clearSearchBarEventMock = new Mock<ClearSearchBarEvent>();
+        _eventAggregatorMock
+            .Setup(ea => ea.GetEvent<SearchRequestedEvent>())
+            .Returns(_searchRequestedEventMock.Object);
+        _eventAggregatorMock
+            .Setup(ea => ea.GetEvent<RefreshRequestedEvent>())
+            .Returns(_refreshRequestedEventMock.Object);
+        _eventAggregatorMock
+            .Setup(ea => ea.GetEvent<ClearSearchBarEvent>())
+            .Returns(_clearSearchBarEventMock.Object);
+        _viewModel = new SearchBarViewModel(_regionManagerMock.Object,_eventAggregatorMock.Object);
+    }
+
+    [Test]
+    public void Test_UploadCommand_Calls_RegionManager()
+    {
+        // Act
+        _viewModel.UploadCommand.Execute(null);
+
+        // Assert
+        _regionManagerMock.Verify(
+            rm => rm.RequestNavigate(
+                "FeedRegion",
+                It.Is<Uri>(u => u.ToString() == "UploadView" && !u.IsAbsoluteUri), 
+                It.IsAny<Action<NavigationResult>>(),It.IsAny<INavigationParameters>()),
+            Times.Once
+        );
+    }
+
+    [Test]
+    public void Test_SearchCommand_RaisesEvent()
+    {
+        // Arrange
+        var searchText = "abc";
+        _viewModel.SearchText = searchText;
+
+        // Act
+        _viewModel.SearchCommand.Execute(null);
+
+        // Assert
+        _searchRequestedEventMock.Verify(
+            e => e.Publish(It.Is<string>(s => s == searchText)),
+            Times.Once);
+
+    }
+
+    [Test]
+    public void Test_RefreshCommand_RaisesEvent()
+    {
+        // Act
+        _viewModel.RefreshCommand.Execute(null);
+
+        // Assert
+        _refreshRequestedEventMock.Verify(
+            e => e.Publish(),
+            Times.Once);
+
+    }
+}
---
+++ Test/TestUploadViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.IO;
+using Database;
+using NUnit.Framework.Legacy;
+using Services;
+using VideoExplorer.Upload;
+
+namespace Test;
+
+[Ignore("Viewmodel has file dialog input and needs to be refactored before UTs")]
+[TestFixture]
+public class TestUploadViewModel
+{
+    private UploadService _mockUploadService;
+    private UploadViewModel _viewModel;
+    private Mock<IVideoRepository> _videoRepositoryMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _videoRepositoryMock = new Mock<IVideoRepository>();
+        _mockUploadService = new UploadService(_videoRepositoryMock.Object);
+        _viewModel = new UploadViewModel(_mockUploadService);
+    }
+
+    [Test]
+    public void Title_IsUpdated_WhenFileIsSelectedAndUploaded()
+    {
+        // Arrange
+        var testFileName = "testVideo.mp4";
+        var fileContent = new MemoryStream();
+        _videoRepositoryMock.Setup(s => s.SaveVideo(It.IsAny<Stream>(),It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(new Uri("http://uploadedvideo.com"));
+
+        // Act
+        _viewModel.UploadFileCommand.Execute(null);
+
+        // Assert
+        ClassicAssert.AreEqual("testVideo", _viewModel.Title);
+        ClassicAssert.AreEqual(testFileName, _viewModel.SelectedFileName);
+        ClassicAssert.AreEqual(100, _viewModel.UploadProgress);
+    }
+
+    [Test]
+    public void UploadedVideoLink_IsUpdated_AfterSuccessfulUpload()
+    {
+        // Arrange
+        var expectedUri = new Uri("http://uploadedvideo.com");
+        _videoRepositoryMock.Setup(s => s.SaveVideo(It.IsAny<Stream>(), It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(expectedUri);
+
+        // Act
+        _viewModel.UploadFileCommand.Execute(null);
+
+        // Assert
+        ClassicAssert.AreEqual(expectedUri.ToString(), _viewModel.UploadedVideoLink);
+    }
+}
